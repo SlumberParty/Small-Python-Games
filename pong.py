c011826db9b6361f820b5652324b080da3198fb7
Claire Cassidy <clacassidy@gmail.com>
2019-11-11 16:37:14 -0800

    screen

---
+++ pong.py
@@ -0,0 +1,12 @@
+import turtle
+
+win = turtle.Screen()
+win.title("Pooong by Claire")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+# main game loop
+
+while True:
+  win.update()
