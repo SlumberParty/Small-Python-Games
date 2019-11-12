1a8bfb1bb84acc07c81d6cf86a9b91558cc39df4
Claire Cassidy <clacassidy@gmail.com>
2019-11-11 16:56:02 -0800

    ball

--- pong.py
+++ pong.py
@@ -25,7 +25,12 @@ paddle_b.penup()
 paddle_b.goto(350, 0)
 
 # Ball
-
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
 
 # main game loop
 while True:
