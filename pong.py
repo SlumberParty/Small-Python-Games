7e42a42648f426c5d7b9802296d060390ccd25ee
Claire Cassidy <clacassidy@gmail.com>
2019-11-11 16:51:53 -0800

    paddle A

--- pong.py
+++ pong.py
@@ -6,7 +6,21 @@ win.bgcolor("black")
 win.setup(width=800, height=600)
 win.tracer(0)
 
-# main game loop
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("hotpink")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
+# Paddle B
+
+
+# Ball
+
+
+# main game loop
 while True:
-  win.update()
+    win.update()
